51991a4d3e8dad2a23456e5470c0865e30435ec6
Stephan Michels <stephan.michels@gmail.com>
2010-10-21 23:57:23 +0200

    Constraint position of one handle to a circle.

--- Canvas.m
+++ Canvas.m
@@ -30,19 +30,31 @@ CGFloat CGFloatClamp(CGFloat value, CGFloat min, CGFloat max) {
 }
 
 - (void)awakeFromNib {
-	handleView1 = [[HandleView handleViewWithPosition:NSMakePoint(100, 100)] retain];
+	handleView1 = [[HandleView handleViewWithPosition:NSMakePoint(100, 50)] retain];
 	handleView1.delegate = self;
 	[self addSubview:handleView1];
 	
-	handleView2 = [[HandleView handleViewWithPosition:NSMakePoint(200, 150)] retain];
+	handleView2 = [[HandleView handleViewWithPosition:NSMakePoint(300, 150)] retain];
 	handleView2.delegate = self;
 	[self addSubview:handleView2];
 }
 
 - (NSPoint)handleView:(HandleView*)handleView willChangePosition:(NSPoint)position {
-	CGRect rect = NSRectToCGRect(self.bounds);
-	return NSMakePoint(CGFloatClamp(position.x, CGRectGetMinX(rect), CGRectGetMaxX(rect)), 
-					   CGFloatClamp(position.y, CGRectGetMinY(rect), CGRectGetMaxY(rect)));
+    if (handleView1 == handleView) {
+        NSPoint center = NSMakePoint(100, 100);
+        CGFloat dx = position.x - center.x;
+        CGFloat dy = position.y - center.y;
+        CGFloat length = hypotf(dx, dy);
+        if (length == 0) {
+            return NSMakePoint(150, 100);
+        }
+        CGFloat radius = 50;
+        return NSMakePoint(center.x + dx * radius / length, center.y + dy * radius / length);
+    } else if (handleView2 == handleView) {
+        CGRect rect = NSRectToCGRect(self.bounds);
+        return NSMakePoint(CGFloatClamp(position.x, CGRectGetMinX(rect), CGRectGetMaxX(rect)), 
+                           CGFloatClamp(position.y, CGRectGetMinY(rect), CGRectGetMaxY(rect)));
+    }
 }
 
 - (void)handleView:(HandleView*)handleView didChangePosition:(NSPoint)position {
@@ -55,6 +67,10 @@ CGFloat CGFloatClamp(CGFloat value, CGFloat min, CGFloat max) {
 	NSRectFill(self.bounds);
 	
 	[[NSColor blackColor] set];
+	
+	NSBezierPath *circlePath = [NSBezierPath bezierPathWithOvalInRect:NSMakeRect(50, 50, 100, 100)];
+	[circlePath stroke];
+	
 	[NSBezierPath strokeLineFromPoint:handleView1.position toPoint:handleView2.position];
 }
 
