dc7e4ceb83dbf61b208ecd8a1605148d1acdff6d
Stephan Michels <stephan.michels@gmail.com>
2012-02-02 09:49:07 +0100

    Added app delegate to close the application if the window was closed.

---
+++ HandleViewAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  HandleViewAppDelegate.h
+//  HandleView
+//
+//  Created by Stephan Michels on 10.09.10.
+//  Copyright (c) 2012 Stephan Michels Softwareentwicklung und Beratung. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HandleViewAppDelegate : NSObject <NSApplicationDelegate>
+
+@end
---
+++ HandleViewAppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  HandleViewAppDelegate.m
+//  HandleView
+//
+//  Created by Stephan Michels on 10.09.10.
+//  Copyright (c) 2012 Stephan Michels Softwareentwicklung und Beratung. All rights reserved.
+//
+
+#import "HandleViewAppDelegate.h"
+
+@implementation HandleViewAppDelegate
+
+#pragma mark - Application delegate
+
+-(void)applicationDidFinishLaunching:(NSNotification *)notification {
+    // Insert code here to initialize your application
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication {
+	return YES;
+}
+
+@end
